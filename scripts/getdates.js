876f56126d66939f2308c599a740de3de1bb3cee
Barxton Keyes <umberon3095@gmail.com>
2023-09-21 17:05:30 -0600

    now

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,14 +1,8 @@
-const pattern = /last_modif\s*=\s*([^;]*)/;
+const modif = document.lastModified;
+console.log(modif);
 
-const lastVisit = parseFloat(document.cookie.replace(pattern, "$1"));
-const lastModif = Date.parse(document.lastModified);
 
-if (Number.isNaN(lastVisit) || lastModif > lastVisit) {
-  document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${
-    location.pathname
-  }`;
 
-  if (isFinite(lastVisit)) {
-    alert("This page has been changed!");
-  }
-}
+
+document.querySelector(".date").textContent= `${modif} the last modified`;
+
