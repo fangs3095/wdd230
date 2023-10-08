e9104b4b109c2ede0dfa0a3d3151784683c54423
Barxton Keyes <umberon3095@gmail.com>
2023-10-07 20:07:33 -0600

    hi

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,10 +1,14 @@
 const modif = document.lastModified;
 console.log(modif);
 
+
+
+
 document.querySelector(".date").textContent= `${modif} the last modified`;
 
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
+
 
 hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('open');
